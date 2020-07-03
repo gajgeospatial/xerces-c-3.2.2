0ca8ee645c41183dfd84be9d4078a403637ec2ff
Glen A Johnson <glen.johnson@gaj-geospatial.com>
2020-07-03 12:28:40 -0400

    Added vs2019 build

---
+++ msvc/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Development/op3d_active/xerces-c-3.2.2
+# Build directory: C:/Development/op3d_active/xerces-c-3.2.2/msvc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("src")
+subdirs("tests")
+subdirs("samples")
---
+++ msvc/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Development/op3d_active/xerces-c-3.2.2/src
+# Build directory: C:/Development/op3d_active/xerces-c-3.2.2/msvc/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
